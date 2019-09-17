01f4db8442d178aae88b7d7fbe866ef6691f25a2
snailcare@protonmail.com <snailcare@protonmail.com>
2019-09-17 19:47:45 +0300

    add db backup

---
+++ server/db_backup/data/staff_20190917194611.sql
@@ -0,0 +1,224 @@
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'1',20,238,'Doctor Michell Tebb');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'2',35,202,'Doctor Pat Desvignes');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'10',8,230,'Doctor Demetris Haldene');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'11',4,183,'Doctor Leland Fridlington');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'12',15,213,'Doctor Madelena Francescotti');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'189',31,228,'Doctor Aubrey Antliff');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'13',27,203,'Doctor Gypsy Macconachy');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'14',35,244,'Doctor Rosette Himsworth');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'15',18,184,'Doctor Benjie Goodbairn');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'16',15,190,'Doctor Neddy DeSousa');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'17',15,226,'Doctor Annalise Antoniak');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'18',8,196,'Doctor Keely Eye');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'19',12,190,'Doctor Berthe Ranscombe');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'100',10,222,'Doctor Jackelyn Inkin');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'101',5,208,'Doctor Simonette Nielson');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'102',5,224,'Doctor Dimitry O''Doohaine');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'103',21,215,'Doctor Jada Casswell');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'104',14,228,'Doctor Ophelie Duncan');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'105',10,234,'Doctor Kitty Kubek');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'106',8,185,'Doctor Leland Foxley');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'107',18,227,'Doctor Dewitt Gavan');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'108',20,223,'Doctor Mareah Peterffy');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'109',14,231,'Doctor Nedda Gouldstraw');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'110',7,229,'Doctor Valina O''Dee');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'111',23,238,'Doctor Venita Watkins');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'112',10,195,'Doctor Emelyne Roy');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'113',32,224,'Doctor Rayner Arpur');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'114',4,183,'Doctor Helga Stollmeyer');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'115',1,212,'Doctor Regine Schubert');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'116',35,186,'Doctor Latashia Swetenham');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'117',27,184,'Doctor Thor Stickford');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'118',20,202,'Doctor Phillipe Hentze');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'119',34,218,'Doctor Tallie Blune');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'120',34,244,'Doctor Boot Jouanny');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'121',23,216,'Doctor Corbet Candy');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'122',14,184,'Doctor Judas Aspole');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'123',29,225,'Doctor Sayres Lighton');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'124',15,171,'Doctor Bili Gordge');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'125',13,180,'Doctor Felix Ingold');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'126',12,240,'Doctor Earlie Delahunty');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'127',29,192,'Doctor Jonis Aubin');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'128',27,237,'Doctor Kittie Dench');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'129',20,188,'Doctor Matthaeus Upwood');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'130',6,213,'Doctor Oralie Messam');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'131',2,211,'Doctor Pier Scrancher');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'132',24,227,'Doctor Marilin Whitehead');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'133',10,189,'Doctor Jessy Dench');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'134',22,178,'Doctor Rich Voaden');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'135',2,207,'Doctor Gerta Willett');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'136',20,176,'Doctor Lela Maultby');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'137',30,193,'Doctor Carline Reyburn');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'138',20,187,'Doctor Jaime Lathan');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'139',4,229,'Doctor Rikki Willans');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'140',8,177,'Doctor Carly Orgel');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'141',27,246,'Doctor Kessiah Petters');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'142',27,220,'Doctor Lebbie Nesbit');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'143',17,202,'Doctor Ellene Ackhurst');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'144',23,222,'Doctor Debera Bazoge');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'145',30,234,'Doctor Ediva Domsalla');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'146',18,245,'Doctor Randolf Kiljan');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'147',22,238,'Doctor Mariam Caffrey');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'148',22,170,'Doctor Pattie Di Biagi');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'149',3,199,'Doctor Giovanni Schwaiger');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'150',20,209,'Doctor Wendie Parrett');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'151',20,245,'Doctor Othilie Rippin');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'152',26,246,'Doctor Madelin Caskey');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'153',34,224,'Doctor Reidar Ensten');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'154',14,222,'Doctor Gabby Oxtoby');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'155',5,247,'Doctor Pauletta Sackur');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'156',11,234,'Doctor Haley Jurca');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'157',26,215,'Doctor Frayda Crayke');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'158',33,192,'Doctor Haroun Vedeshkin');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'159',3,223,'Doctor Abbe Chappel');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'160',10,234,'Doctor Shandy D''Acth');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'161',4,235,'Doctor Petronilla Duckham');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'162',5,186,'Doctor Beatriz Clemoes');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'163',33,213,'Doctor Cherianne Bhar');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'164',14,177,'Doctor Delmer Danzig');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'165',26,195,'Doctor Hadria Smitten');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'166',35,173,'Doctor Erasmus Claridge');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'167',33,183,'Doctor Micheil Prandi');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'168',21,219,'Doctor Lissie Grange');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'169',20,190,'Doctor Mellisa Boig');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'170',2,243,'Doctor Currie Muff');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'171',28,227,'Doctor Luelle Raybould');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'172',12,188,'Doctor Giffy Sorro');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'173',28,215,'Doctor Janeta Ravenscroft');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'174',10,182,'Doctor Vite Alflatt');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'175',34,240,'Doctor Nathaniel Ca');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'176',23,201,'Doctor Lina Tilston');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'177',27,179,'Doctor Reggis McGairl');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'178',21,231,'Doctor Torie Beran');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'179',9,202,'Doctor Rutter Coller');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'180',30,209,'Doctor Hagen Carlton');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'181',6,193,'Doctor Livy Esterbrook');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'182',29,199,'Doctor Norby Thieme');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'183',20,207,'Doctor Tabina Cleobury');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'184',4,247,'Doctor Gwenny Gasticke');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'185',8,174,'Doctor Judye O''Lochan');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'186',24,206,'Doctor Angel Kelloway');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'187',15,234,'Doctor Blanch Boyford');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'188',33,220,'Doctor Chantal Vannuccini');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'190',22,229,'Doctor Rozina Moat');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'191',18,210,'Doctor Rena Wilsee');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'192',7,206,'Doctor Fania Bullerwell');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'193',25,216,'Doctor Eldridge Oldfield');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'194',22,227,'Doctor Bernie Casey');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'195',5,171,'Doctor Quinta Monument');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'196',3,224,'Doctor Wallie Sharpous');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'197',13,174,'Doctor Celeste Davidovich');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'198',5,174,'Doctor Gwendolyn Jamary');
+INSERT INTO snailcare.staff (id,profession,branch,personal_information) VALUES (
+'199',1,237,'Doctor Tobit Aked');
